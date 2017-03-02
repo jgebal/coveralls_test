096793cfb5e2204145bf73132b16a43e17106bcb
Jacek Gębal <jgebal@gmail.com>
2017-03-02 22:10:44 +0000

    bump

--- push_to_coveralls.sh
+++ push_to_coveralls.sh
@@ -6,5 +6,9 @@ service_pull_request=$TRAVIS_PULL_REQUEST
 service_branch=$TRAVIS_BRANCH
 
 #https://coveralls.io/api/v1/jobs
-curl -v -X POST https://coveralls.io/api/v1/jobs -H "Content-Type: application/json" -d @"coverage.json"
+curl -v -X POST https://coveralls.io/api/v1/jobs -d @"coverage.json" \
+ -H "Content-Type: application/json" \
+ -H "service_job_id: $TRAVIS_JOB_ID" \
+ -H "service_name: 'travis-ci'" \
+
 
