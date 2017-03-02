35664f56737a7cf9b257227d0dfdeeb236cbb46d
Jacek Gębal <jgebal@gmail.com>
2017-03-02 23:32:59 +0000

    bump

--- push_to_coveralls.sh
+++ push_to_coveralls.sh
@@ -9,7 +9,9 @@ echo '$TRAVIS_JOB_ID'=$TRAVIS_JOB_ID
 
 #sed -i -- "s/\"name\":/\"source_digest\":\"3abb17b66815bc7946cefe727737d295\",\"name\":/g" coverage.json
 #sed -i -- "s/\"name\":/\"source\":\"\",\"name\":/g" coverage.json
-sed -i -- "s/\"source_files\":/\"service_job_id\":\"$TRAVIS_JOB_ID\",\"service_name\":\"travis-ci\", \"source_files\":/g" coverage.json
+
+#sed -i -- "s/\"source_files\":/\"service_job_id\":\"$TRAVIS_JOB_ID\",\"service_name\":\"travis-ci\", \"source_files\":/g" coverage.json
+sed -i -- "s/\"source_files\":/\"repo_token\":\"$COVERALLS_REPO_TOKEN\",\"service_job_id\":\"$TRAVIS_JOB_ID\",\"service_name\":\"travis-ci\", \"source_files\":/g" coverage.json
 
 cat coverage.json
 #https://coveralls.io/api/v1/jobs
