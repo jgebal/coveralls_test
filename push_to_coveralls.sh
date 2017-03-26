b6f3a3f4088c6059ea5a0789addb31f29043efcf
Jacek Gębal <jgebal@gmail.com>
2017-03-26 00:40:46 +0000

    Update push_to_coveralls.sh

--- push_to_coveralls.sh
+++ push_to_coveralls.sh
@@ -5,8 +5,9 @@
 #service_pull_request=$TRAVIS_PULL_REQUEST
 #service_branch=$TRAVIS_BRANCH
 
-echo '$TRAVIS_JOB_ID'=$TRAVIS_JOB_ID
-echo '$TRAVIS_JOB_NUMBER'=$TRAVIS_JOB_NUMBER
+echo '$TRAVIS_JOB_ID='$TRAVIS_JOB_ID
+echo '$TRAVIS_JOB_NUMBER='$TRAVIS_JOB_NUMBER
+echo '$IS_BUILD_RELEASE_JOB='$IS_BUILD_RELEASE_JOB
 
 #sed -i -- "s/\"name\":/\"source_digest\":\"3abb17b66815bc7946cefe727737d295\",\"name\":/g" coverage.json
 #sed -i -- "s/\"name\":/\"source\":\"\",\"name\":/g" coverage.json
